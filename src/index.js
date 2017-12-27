1732dc14fe944c608f375e2f775b6eef4e0d881b
Andrew <andrew.vinocur@gmail.com>
2017-12-27 15:26:25 -0800

    adds import statement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+
 // Create a new component. This component should produce some HTML
 const App = function() {
   return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it on the page (i.e. in the DOM)
+React.render(App);
