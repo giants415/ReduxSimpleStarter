d40f59dca7ae8e9c8b0b450514c73504ab2df6ff
Andrew <andrew.vinocur@gmail.com>
2017-12-29 18:08:58 -0800

    refactored to use es6 to clean up event trigger

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
+            // ( ) around event are dropped in this case bc only 1 argument is passed in
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 
-  //event handler
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  //event handler. Commented out and refactored to
+  //take advantage of es6
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
