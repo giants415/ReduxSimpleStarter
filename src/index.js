bcd2962361d15bff156b80cea62315f0aac9b865
Andrew <andrew.vinocur@gmail.com>
2018-01-02 15:15:35 -0800

    imports and adds video detail to App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 import KEY from './config';
 
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail />
         <VideoList videos={this.state.videos} />
       </div>
     );
