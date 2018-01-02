f411d4b6323ce8368d5e5bbc6100af1a3e49c9f4
Andrew <andrew.vinocur@gmail.com>
2018-01-02 13:35:32 -0800

    structures out video item component to use more info from YT API

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,8 +3,19 @@ import React from 'react';
 const VideoListItem = ({video}) => {
   // const video = props.video; this line is replaced, in es6,
   //by the curly brace var the is passed in
-  
-  return <li>Video</li>
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading"></div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
