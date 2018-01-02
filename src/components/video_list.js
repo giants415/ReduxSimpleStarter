1642cf2682f387b43c349d2271e059399fcca16f
Andrew <andrew.vinocur@gmail.com>
2018-01-02 13:19:47 -0800

    imports video list and creates videoItems to display data

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import VideoListItem from './video_list_item';
+
 //since this is a functional component, props MUST BE PASSED IN
 //class components have props available throughout the component
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
-  )
-}
+  );
+};
 
 export default VideoList;
