fd9bbe24294bf9dbad5836fe3219c1329127cb45
Andrew <andrew.vinocur@gmail.com>
2018-01-02 15:59:03 -0800

    adds fn to to pass selected video to video_list_item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,7 +6,12 @@ import VideoListItem from './video_list_item';
 //class components have props available throughout the component
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return (
+      <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video} />
+    )
   });
 
   return (
