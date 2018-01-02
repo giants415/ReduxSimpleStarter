87ea11133dd7a1ee20b7be513c0ae8d13ab117bf
Andrew <andrew.vinocur@gmail.com>
2018-01-02 13:00:22 -0800

    adds comment and passes prop into component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const VideoList = () => {
+//since this is a functional component, props MUST BE PASSED IN
+//class components have props available throughout the component
+const VideoList = (props) => {
   return (
     <ul className="col-md-4 list-group">
+      {props.videos.length}
     </ul>
   )
 }
