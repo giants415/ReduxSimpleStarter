b6ac228fa45ccf7c0ace43df6996f0b193f9918d
Andrew <andrew.vinocur@gmail.com>
2018-01-02 13:37:57 -0800

    brings in thumbnail and title info from YTSearch

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,15 +3,16 @@ import React from 'react';
 const VideoListItem = ({video}) => {
   // const video = props.video; this line is replaced, in es6,
   //by the curly brace var the is passed in
+  const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imageUrl} />
         </div>
         <div className="media-body">
-          <div className="media-heading"></div>
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
     </li>
