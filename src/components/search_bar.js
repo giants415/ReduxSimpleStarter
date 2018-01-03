53d8a425f79a4799a92ae92162196e6970035dfc
Andrew <andrew.vinocur@gmail.com>
2018-01-02 16:18:20 -0800

    clears out comments and refactors onInputChange outside of onChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,21 +10,19 @@ class SearchBar extends Component {
   //controlled form input - onchange causes component to rerender
   //and update value seen in search bar
   render() {
-          // ( ) around event are dropped in this case bc only 1 argument is passed in
     return (
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
 
-  //event handler. Commented out and refactored to
-  //take advantage of es6
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
